fa1c0f63240a7e0dfa04334b8b64486260b92b91
Ruairi O Brien <ruairitobrien@gmail.com>
2018-04-21 18:57:57 +0100

    Added pm2

--- server/boot/login.js
+++ server/boot/login.js
@@ -1,28 +1,26 @@
-"use strict";
-
-const SpotifyWebApi = require("spotify-web-api-node");
-
-const scopes = ["user-read-private", "user-read-email"];
-const redirectUri = "http://localhost:3000/callback";
-const clientId = "ee4aa78cde4c4be08978d79c180e11c9";
-const clientSecret = "acfc43102e5c4e05902e66284dfdcb19";
+'use strict';
+const SpotifyWebApi = require('spotify-web-api-node');
+const scopes = ['user-read-private', 'user-read-email'];
+const redirectUri = 'http://localhost:3000/callback';
+const clientId = 'ee4aa78cde4c4be08978d79c180e11c9';
+const clientSecret = 'acfc43102e5c4e05902e66284dfdcb19';
 
 module.exports = function(app) {
   var router = app.loopback.Router();
-  router.get("/login", function(req, res) {
+  router.get('/login', function(req, res) {
     const spotifyApi = new SpotifyWebApi({
       redirectUri: redirectUri,
-      clientId: clientId
+      clientId: clientId,
     });
     var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
     res.redirect(authorizeURL);
   });
 
-  router.get("/callback", function(req, res) {
+  router.get('/callback', function(req, res) {
     var credentials = {
       clientId: clientId,
       clientSecret: clientSecret,
-      redirectUri: redirectUri
+      redirectUri: redirectUri,
     };
 
     var spotifyApi = new SpotifyWebApi(credentials);
@@ -33,44 +31,36 @@ module.exports = function(app) {
     // Retrieve an access token and a refresh token
     spotifyApi.authorizationCodeGrant(code).then(
       function(data) {
-        console.log("The token expires in " + data.body["expires_in"]);
-        console.log("The access token is " + data.body["access_token"]);
-        console.log("The refresh token is " + data.body["refresh_token"]);
-
-        // Set the access token on the API object to use it in later calls
-        spotifyApi.setAccessToken(data.body["access_token"]);
-        spotifyApi.setRefreshToken(data.body["refresh_token"]);
-        res.cookie("access_token", data.body["access_token"]);
-        res.cookie("refresh_token", data.body["refresh_token"]);
-        res.redirect("http://localhost:3001/callback");
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+        res.cookie('access_token', data.body['access_token']);
+        res.cookie('refresh_token', data.body['refresh_token']);
+        res.redirect('http://localhost:3001/callback');
       },
       function(err) {
-        console.log("Something went wrong!", err);
+        console.log('Something went wrong!', err);
         res.send(err);
       }
     );
-
   });
 
-  router.get("/refresh", function(req, res) {
+  router.get('/refresh', function(req, res) {
     const spotifyApi = new SpotifyWebApi({
       redirectUri: redirectUri,
       clientId: clientId,
-      clientSecret: clientSecret
+      clientSecret: clientSecret,
     });
-    spotifyApi.setRefreshToken(req.get("refresh_token"));
+    spotifyApi.setRefreshToken(req.get('refresh_token'));
     // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
     spotifyApi.refreshAccessToken().then(
       function(data) {
-        console.log("The access token has been refreshed!");
-
-        // Save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body["access_token"]);
-        res.cookie("access_token", data.body["access_token"]);
-        res.send({access_token: data.body["access_token"]});
+        console.log('The access token has been refreshed!');
+        spotifyApi.setAccessToken(data.body['access_token']);
+        res.cookie('access_token', data.body['access_token']);
+        res.send({access_token: data.body['access_token']});
       },
       function(err) {
-        console.log("Could not refresh access token", err);
+        console.log('Could not refresh access token', err);
       }
     );
   });
