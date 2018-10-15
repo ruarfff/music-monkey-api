bb7b1f9be3e113ec5bbf8988108311a2ecb17587
Balyko Eugene <balyko.peaceit@gmail.com>
2018-10-15 17:48:13 +0300

    get right userId

--- src/rsvp/rsvpNotifier.ts
+++ src/rsvp/rsvpNotifier.ts
@@ -3,5 +3,5 @@ import { send } from '../notification'
 
 export const onRsvpSaved = (rsvp: IRsvp, userId: string) => {
   send('mm-rsvps-' + rsvp.eventId, 'rsvp-saved', rsvp)
-  send('mm-user-notifications-' + userId, 'rsvp-saved', rsvp)
+  send('mm-user-notifications-' + userId, 'notifications-saved', rsvp)
 }
