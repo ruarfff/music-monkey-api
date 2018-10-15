257645a6f4d85306c516e21aa4b79fb13ed3bb53
Balyko Eugene <balyko.peaceit@gmail.com>
2018-10-15 16:41:32 +0300

    update guests by event id

--- src/rsvp/rsvpNotifier.ts
+++ src/rsvp/rsvpNotifier.ts
@@ -2,6 +2,6 @@ import { IRsvp } from '../model'
 import { send } from '../notification'
 
 export const onRsvpSaved = (rsvp: IRsvp) => {
-  send('mm-rsvps-' + rsvp.userId, 'rsvp-saved', rsvp)
+  send('mm-rsvps-' + rsvp.eventId, 'rsvp-saved', rsvp)
   send('mm-user-notifications-' + rsvp.userId, 'rsvp-saved', rsvp)
 }
