9f7837315319168f38f4998ebc902ad2069ba7a0
Balyko Eugene <balyko.peaceit@gmail.com>
2018-10-25 16:21:59 +0300

    notification enchantment

---
+++ src/model/notification.ts
@@ -0,0 +1,48 @@
+export const notifications = [
+  {
+    userId: 'host-userId',
+    type: 'rsvp',
+    context: 'event',
+    contextId: 'eventId1',
+    text: 'Some Person is going to Event X.',
+    status: 'Unread'
+  },
+  {
+    userId: 'host-userId',
+    type: 'rsvp',
+    context: 'event',
+    contextId: 'eventId2',
+    text: 'Some Person is going to Event X.',
+    status: 'Unread'
+  },
+  {
+    userId: 'host-userId',
+    type: 'rsvp',
+    context: 'event',
+    contextId: 'eventId3',
+    text: 'Some Person is going to Event X.',
+    status: 'Unread'
+  }
+]
+
+export const createNotification = (data: any, option: string) => {
+  const {
+    userId,
+    event,
+  } = data
+  switch (option) {
+    case 'rsvp':
+      notifications.push({
+        userId,
+        type: 'rsvp',
+        context: event,
+        contextId: event.eventId,
+        text: `Some Person is going to Event ${event.name}`,
+        status: 'Unread'
+      })
+  }
+}
+
+export const getNotificationByUserId = (userId: string) => {
+  return notifications.filter(n => n.userId === userId)
+}
--- src/notification/notificationRoutes.ts
+++ src/notification/notificationRoutes.ts
@@ -1,41 +1,19 @@
 import { Request, Response, Router } from 'express'
 import * as passport from 'passport'
+import { getNotificationByUserId } from '../model/notification'
 
 const router = Router()
-
-const mockData = [
-  {
-    userId: 'host-userId',
-    type: 'rsvp',
-    context: 'event',
-    contextId: 'eventId1',
-    text: 'Some Person is going to Event X.',
-    status: 'Unread'
-  },
-  {
-    userId: 'host-userId',
-    type: 'rsvp',
-    context: 'event',
-    contextId: 'eventId2',
-    text: 'Some Person is going to Event X.',
-    status: 'Unread'
-  },
-  {
-    userId: 'host-userId',
-    type: 'rsvp',
-    context: 'event',
-    contextId: 'eventId3',
-    text: 'Some Person is going to Event X.',
-    status: 'Unread'
-  }
-]
 
 router.get(
   '/:userId',
   passport.authenticate('jwt', { session: false }),
   (req: Request, res: Response) => {
-    console.log(req)
-    res.send(mockData)
+    try {
+      const userNotifications = getNotificationByUserId(req.params.userId)
+      res.send(userNotifications)
+    } catch (e) {
+      res.status(500).send(e.message)
+    }
   }
 )
 
