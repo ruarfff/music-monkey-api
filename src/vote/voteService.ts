c1ac7c13d3551ee96a85b222ff55962b151b3d25
Ruairi O Brien <ruairitobrien@gmail.com>
2020-02-11 00:13:51 +0000

    Fixed like tracks endoint

--- src/vote/voteService.ts
+++ src/vote/voteService.ts
@@ -22,11 +22,25 @@ export const getVotesWithStatus = async (eventId: string, userId: string) => {
 
 export const getVotesByUserWithTracks = async (user: IUser) => {
   const votes = await getVotesByUserId(user.userId)
+  let allTracks: ITrack[] = []
   let trackIds = votes.map(v => v.trackId.split(':')[2])
   trackIds = trackIds.filter((item, i) => trackIds.indexOf(item) === i)
+  if (trackIds.length <= 50) {
+    const { tracks } = await getMultipleTracks(trackIds, user)
+    allTracks = tracks
+  } else {
+    const chunk = 50
+    let i
+    let j
+    let chunkedTrackIds
+    for (i = 0, j = trackIds.length; i < j; i += chunk) {
+      chunkedTrackIds = trackIds.slice(i, i + chunk)
+      const { tracks } = await getMultipleTracks(chunkedTrackIds, user)
+      allTracks = [...allTracks, ...tracks]
+    }
+  }
   const trackMap = new Map<string, ITrack>()
-  const { tracks } = await getMultipleTracks(trackIds, user)
-  tracks.forEach(t => {
+  allTracks.forEach(t => {
     trackMap.set(t.id, t)
   })
   return votes.map(v => ({
